165e20f519e93fb0b0fa643ac91c15a86a23e901
noy248 <noya248@gmail.com>
2018-04-19 12:33:01 +0300

    Changing new-student options: adding average grade and validating the id in the DB.
    Changing searchStudent to searching by Name or by ID.

--- client/app/students/new-student/new-student.controller.js
+++ client/app/students/new-student/new-student.controller.js
@@ -4,6 +4,7 @@ angular.module('classify')
     .controller('AddStudent', function ($scope, $mdDialog,$students,$q, $timeout) {
         $scope.student = {name: {}, gender: 'female', avgGrade: 0, prefer: {}};
         $scope.simulateQuery=true;
+        $scope.validID = true;
         $scope.searchStds = [];
         $scope.searchQuery= {
             sort: 'name.first',
@@ -13,6 +14,24 @@ angular.module('classify')
 
         $scope.save = function () {
             $mdDialog.hide($scope.student);
+        };
+
+        $scope.validateID = function(id){
+            if  (id){
+                if (id.length == 9) {
+                    $students.search({
+                        sort: 'name.first',
+                        limit: 10,
+                        page: 1,
+                        id:id
+                    }).$promise.then(function (items) {
+                        if(items.docs.length > 0) {
+                            $scope.validID = false;
+                        }else
+                            $scope.validID =  true;
+                    });
+                }
+            }
         };
 
         /* Preference section */
