de89c8ccda2f08fff4b4da8f3f547393ba29ab1c
Eden Ben Shoshan <eden47@gmail.com>
2018-05-19 17:15:50 +0300

    add mutation, crossover and social fitness

--- server/api/class/alg/fitness.js
+++ server/api/class/alg/fitness.js
@@ -2,30 +2,40 @@ const {students, maxStudents} = JSON.parse(process.env.CLASSIFY_PARAMS);
 const _ = require('lodash');
 const sd = require('standard-deviation');
 
-const GRADE_DIFF_THRESHOLD = 2;
-const GRADE_DEVIATION_FACTOR = 0.2;
-
-const all = {
-    avgGrade: _.meanBy(students, 'avgGrade')
+const types = {
+    grade: {
+        all: _.meanBy(students, 'avgGrade'),
+        field: 'avgGrade',
+        DIFF_THRESHOLD: 2,
+        DIFF_PENALTY_FACTOR: 0.1,
+        DEVIATION_PENALTY_FACTOR: 0.2
+    },
+    social: {
+        all: _.meanBy(students, 'social'),
+        field: 'social',
+        DIFF_THRESHOLD: 0.1,
+        DIFF_PENALTY_FACTOR: 2,
+        DEVIATION_PENALTY_FACTOR: 2
+    }
 };
 
-const getGradesFitness = classes => {
+const getFitness = (classes, type) => {
     // Difference between each class' average grade to global average grade
-    const classesAvgGradesDiffFromAll = classes.map(c => Math.abs(all.avgGrade - _.meanBy(c.students, 'avgGrade')));
+    const classesAvgGradesDiffFromAll = classes.map(c => Math.abs(type.all - _.meanBy(c.students, type.field)));
 
     // Ex: Difference of 1.5 would go unpunished, a difference of 3 bears a penalty of 0.1 ((3 - THRESHOLD) / 10)
-    const avgGradePenalties = classesAvgGradesDiffFromAll.map(grade => grade <= GRADE_DIFF_THRESHOLD ? 0 : (grade - GRADE_DIFF_THRESHOLD) / 10);
+    const avgGradePenalties = classesAvgGradesDiffFromAll.map(grade => grade <= type.DIFF_THRESHOLD ? 0 : (grade - type.DIFF_THRESHOLD) * type.DIFF_PENALTY_FACTOR);
     const avgGradeFitness = _.sum(avgGradePenalties) > 1 ? 0 : 1 - _.sum(avgGradePenalties);
 
     console.log('====== Grades');
-    console.log('All: ', all.avgGrade);
+    console.log('All: ', type.all);
     console.log('Diffs from all: ', classesAvgGradesDiffFromAll);
     console.log('Penalties: ', avgGradePenalties);
     console.log('Fitness: ' + avgGradeFitness);
 
     // Calculate the deviation of the students' grades in each class
     // (In order to learn if students are divided the same way around the mean in all classes)
-    const classesGradesDeviations = classes.map(c => sd(_.map(c.students, 'avgGrade')));
+    const classesGradesDeviations = classes.map(c => sd(_.map(c.students, type.field)));
 
     // Calculate the deviation of the classes' grades' deviations in order to learn if the current division is fitted
     const classesGradesDeviationDifference = sd(classesGradesDeviations);
@@ -35,7 +45,7 @@ const getGradesFitness = classes => {
     // calculate 1 - the result in order to get how fitted the arrangement is (little deviation means better results)
     // [ 18.11874413111706, 19.503390018717827, 19.3416361481879 ] -> 0.6181399908054543 -> 0.8763720018389092
     // [ 18.65252440466616, 12.398207755782911, 19.08777968579199 ] -> 3.0560740339892782 -> 0.38878519320214433
-    let gradesDeviationFitness = 1 - classesGradesDeviationDifference * GRADE_DEVIATION_FACTOR;
+    let gradesDeviationFitness = 1 - classesGradesDeviationDifference * type.DEVIATION_PENALTY_FACTOR;
     gradesDeviationFitness = gradesDeviationFitness < 0 ? 0 : gradesDeviationFitness;
 
     console.log('====== Grades Deviations');
@@ -47,6 +57,9 @@ const getGradesFitness = classes => {
 };
 
 module.exports = classes => {
-    const gradeFitness = getGradesFitness(classes);
-    return gradeFitness;
+    /*const gradeFitness = getFitness(classes, types['grade']);
+    const socialFitness = getFitness(classes, types['social']);
+
+    return gradeFitness * 0.6 + socialFitness * 0.4;*/
+    return getFitness(classes, types['social']);
 };
--- server/api/class/alg/index.js
+++ server/api/class/alg/index.js
@@ -15,10 +15,60 @@ console.log(fitnessFunction(firstClasses));
 
 const config = {
     mutationFunction: classes => {
+        classes = _.shuffle(classes);
+
+        if (_.random(10) < 3) {
+            const maxClass = _.maxBy(classes, c => c.students.length);
+            const minClass = _.minBy(_.without(classes, maxClass), c => c.students.length);
+
+            const student = _.sample(maxClass.students);
+            _.pull(maxClass.students, student);
+            minClass.students.push(student);
+        }
+        else {
+            const firstClass = _.sample(classes);
+            const secondClass = _.sample(_.without(classes, firstClass));
+            const firstStudent = _.sample(firstClass.students);
+            const secondStudent = _.sample(secondClass.students);
+
+            _.pull(firstClass.students, firstStudent);
+            _.pull(secondClass.students, secondStudent);
+            firstClass.students.push(secondStudent);
+            secondClass.students.push(firstStudent);
+        }
+
         return classes;
     },
     crossoverFunction: (classesA, classesB) => {
-        return [classesA, classesB];
+        const firstHalf = _.slice(students, 0, Math.floor(students.length / 2));
+        const secondHalf = _.slice(students, Math.floor(students.length / 2), students.length);
+        const classes = _.cloneDeep(classesA);
+        _.each(classes, c => c.students = []);
+
+        _.each([firstHalf, secondHalf], (half, i) => {
+            const arrangement = i === 0 ? classesA : classesB;
+
+            _.each(half, student => {
+                const _class = _.find(arrangement, c => {
+                    return !!_.find(c.students, {id: student.id});
+                });
+
+                const classToPutIn = _.find(classes, {index: _class.index});
+                classToPutIn.students.push(student);
+            });
+        });
+
+        _.each(classes, c => {
+            while (c.students.length > studentsPerClass) {
+                const minClass = _.minBy(classes, c => c.students.length);
+
+                const student = _.sample(c.students);
+                _.pull(c.students, student);
+                minClass.students.push(student);
+            }
+        });
+
+        return [classes];
     },
     fitnessFunction,
     doesABeatBFunction: (classesA, classesB) => {
@@ -34,6 +84,13 @@ const config = {
 
 module.exports = function () {
     const algorithm = GeneticAlgorithm(config);
+    /*console.log(algorithm.bestScore());
+    _.times(30, () => {
+        console.log('======');
+        algorithm.evolve();
+        console.log(algorithm.bestScore());
+    });*/
+
     /*
      _.times(10, () => {
      algorithm.evolve();
